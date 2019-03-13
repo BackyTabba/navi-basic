6b586fd20d2084e15671d55b3f447db60b77e9b4
jonas <j_fock06@uni-muenster.de>
2019-03-13 14:24:34 +0100

    Überarbeitung der Methoden und dokumentation in den Klassen Input und Route.

--- src/test/java/Input.java
+++ src/test/java/Input.java
@@ -1,13 +1,31 @@
+/*
+The Class Input stores the userinput given to run the API
+ */
 public class Input{
+
+    /*
+    Deklarierung der Variablen und Objekte
+    Deklaration and initialisation of needed variables and objects.
+    @double x1, x2, y1, y2 are Coordinates witch will be set in the Konstruktor.
+    @Route route is the Route-object, witch will be dragged through the Programm and ejected at the End.
+     */
     private double x1,x2,y1,y2;
+    private Route route = new Route();
+
+    /*
+    The Konstruktor asks and initializes the needed variables for the rout-computing in the external API.
+    @double x1, x2, y1, y2 should be the start(x1,y1) and the finish(x2,y2) coordinates.
+     */
     public Input (double x1, double x2, double y1, double y2){
         this.x1=x1;
         this.x2=x2;
         this.y1=y1;
         this.y2=y2;
     }
+
     /*
-    returns Array with values : [x1,x2,y1,y2]
+    Returns Array with values : {x1,x2,y1,y2}
+    @return double {x1,x2,y1,y2}
      */
     public double[] getValues(){
         double[] ruck = {x1,x2,y1,y2};
--- src/test/java/Route.java
+++ src/test/java/Route.java
@@ -1,20 +1,40 @@
-import java.util.ArrayList;
-import java.util.List;
+/*
+Needed archives are imported here.
+@java.util.ArrayList
+@java.util.List
+ */
+//import java.util.ArrayList;
+//import java.util.List;
 
+/*
+This Class Contains the route-detials wich are ejected to the user in the end.
+ */
 public class Route {
+
+    /*
+    Declaration and inizialisation of the needed variables
+    @List<String> RoutenStruktur will contain informations given about the route.
+     */
     private List<String> RoutenStruktur = new ArrayList<String>();
 
+    /*
+    This is the Konstruktor. It does nothing.
+     */
     public Route(){
 
-
     }
+
     /*
-    returns ein List<String>-Objekt, dass die Routenanweisungen enthält
+    Returns RoutenStruktur.
+    @return List<String> RoutenStruktur
      */
     public List<String> getRoutenStruktur(){
         return RoutenStruktur;
     }
-
+    /*
+    Sets the RoutenStruktur.
+    @List<String> pRoutenStrukur the RoutenStruktur to set.
+     */
     public void setRoutenStruktur(List<String> pRoutenStruktur){
         this.RoutenStruktur = pRoutenStruktur;
     }
