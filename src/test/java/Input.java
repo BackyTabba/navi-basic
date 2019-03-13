de18806f57387d65766dcde6f5c934da414e6417
jonas <j_fock06@uni-muenster.de>
2019-03-13 14:51:06 +0100

    Überarbeitung der Methoden und dokumentation in den Klassen Input und Route.

--- src/test/java/Input.java
+++ src/test/java/Input.java
@@ -8,19 +8,35 @@ public class Input{
     Deklaration and initialisation of needed variables and objects.
     @double x1, x2, y1, y2 are Coordinates witch will be set in the Konstruktor.
     @Route route is the Route-object, witch will be dragged through the Programm and ejected at the End.
+    @vehicle will be the used vehicle.
      */
     private double x1,x2,y1,y2;
+    private String vehicle;
     private Route route = new Route();
 
     /*
-    The Konstruktor asks and initializes the needed variables for the rout-computing in the external API.
+    The Konstruktor asks and initializes the needed variables for the rout-computing in the external API, then it opens creates Test_API-object.
     @double x1, x2, y1, y2 should be the start(x1,y1) and the finish(x2,y2) coordinates.
+    @String vehicle should be the used vehicle.
      */
-    public Input (double x1, double x2, double y1, double y2){
+    public Input (double x1, double x2, double y1, double y2, String vehicle){
         this.x1=x1;
         this.x2=x2;
         this.y1=y1;
         this.y2=y2;
+        this.vehicle=vehicle;
+
+        String startX = String.valueOf(this.x1);
+        String startY = String.valueOf(this.y1);
+
+        String destinationX = String.valueOf(this.x2);
+        String destinationY = String.valueOf(this.y2);
+
+
+        String start = startX + "," + startY;
+        String destination = destinationX + "," + destinationY;
+
+        Request(start, destination, this.vehicle);
     }
 
     /*
